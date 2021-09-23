6f09d03ccf421c8cfccdd53499498461daa74381
Angie Pineda <agpinedam@unal.edu.co>
2021-09-23 10:01:43 -0500

    Readme edited

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,16 +1,8 @@
-# DB_Challenge
-
-## Creation of DB people H2
-
-
-Using mySQL Workench, we have written the following instructions for the creation of the employee, company, children, instituttion, and degree tables, additionally they were filled with some values with the following commands:
-
-````sql
 CREATE DATABASE PEOPLE;
 USE PEOPLE; 
-````
 
-````sql
+Show Tables;
+
 CREATE TABLE Company (
 	idCompany	INT PRIMARY KEY,
 	coName		VARCHAR(45) NOT NULL,
@@ -18,9 +10,7 @@ CREATE TABLE Company (
 	email		VARCHAR(45) NOT NULL,
 	address		VARCHAR(45)
 );
-````
 
-````sql
 CREATE TABLE employee (
 	idEmployee	INT NOT NULL,
 	firstName	VARCHAR(45) NOT NULL,
@@ -35,10 +25,8 @@ CREATE TABLE employee (
 	CONSTRAINT 	FK_idCompany  FOREIGN KEY (idCompany)  
 			REFERENCES Company  (idCompany) 
 			ON DELETE CASCADE ON UPDATE CASCADE
-);   
-````
-
-````sql
+);  	
+                    
 CREATE TABLE children (
 	idChild		INT AUTO_INCREMENT PRIMARY KEY,
 	idEmployee	INT NOT NULL,
@@ -50,9 +38,7 @@ CREATE TABLE children (
 			ON DELETE CASCADE
 			ON UPDATE CASCADE
 );
-````
 
-````sql
 CREATE TABLE institution (
 	idInstitution	INT NOT NULL,
 	idEmployee	INT NOT NULL,
@@ -62,9 +48,8 @@ CREATE TABLE institution (
                     	ON DELETE CASCADE
 			ON UPDATE CASCADE
 );
-````
 
-````sql
+# add values
 INSERT INTO Company (idCompany, coName, phoneNumber, email, address) VALUES ('1', 'Ara', ' 54136579', 'ara@gmail.com', ' 72 Lancaster rd');
 INSERT INTO Company (idCompany, coName, phoneNumber, email, address) VALUES ('2', 'Olimpica', '54335486', 'olimpic@gmail.com', '81 Stevenson st');
 INSERT INTO Company (idCompany, coName, phoneNumber, email, address) VALUES ('3', 'Only', '487257', 'only@gmail.com', '12 Oxford st.');
@@ -83,25 +68,15 @@ INSERT INTO Children (idChild, idEmployee, fullName, birthDate, gender) VALUES (
 INSERT INTO Children (idChild, idEmployee, fullName, birthDate, gender) VALUES ('4', '74', 'Goten', '2016-07-28', 'Male');
 INSERT INTO Children (idChild, idEmployee, fullName, birthDate, gender) VALUES ('5', '92', 'Maron', '2017-10-01', 'Female');
 
+
 INSERT INTO Institution (idInstitution, idEmployee, institutionName) VALUES ('1', '74', 'Harvard University');
-INSERT INTO Institution (idInstitution, idEmployee, institutionName) VALUES ('2', '95', 'Stanford University');
-INSERT INTO Institution (idInstitution, idEmployee, institutionName) VALUES ('3', '83', 'University of Cambridge');
-INSERT INTO Institution (idInstitution, idEmployee, institutionName) VALUES ('4', '74', 'Princeton University');
-INSERT INTO Institution (idInstitution, idEmployee, institutionName) VALUES ('5', '92', 'Massachusetts Institute of Technology');
-````
+INSERT INTO Institution (idInstitution, idEmployee, institutionName) VALUES ('2', '83', 'Stanford University');
+INSERT INTO Institution (idInstitution, idEmployee, institutionName) VALUES ('3', '92', 'University of Cambridge');
+INSERT INTO Institution (idInstitution, idEmployee, institutionName) VALUES ('4', '95', 'Princeton University');
+INSERT INTO Institution (idInstitution, idEmployee, institutionName) VALUES ('5', '64', 'Massachusetts Institute of Technology');
 
-### Relations between tables
-
-![plot](relations.png)
-
-### SQL commands
-
-* Select and join Employee and Instituttion tables.
-````sql
- SELECT em.*, ins.* From Employee em INNER JOIN Institution ins on em.idEmployee = ins.idEmployee;
-````
-
-* Bring the information of the children "Children table" whose parents work in PriceSmart which has an idCompany = 5. Note: Just bring the information from the Children table.
-````sql
+SELECT em.*, ins.* From Employee em INNER JOIN Institution ins on em.idEmployee = ins.idEmployee;
+#Traer la información de los niños “Tabla Children” cuyos papás Trabajan en PriceSmart el cual tiene un idCompany = 5. Nota: solo traer la información de la tabla Children.
  SELECT * From Children Where idEmployee in (SELECT idEmployee From Employee Where idCompany in (SELECT idCompany From Company Where idCompany = 5));
-````
+
+
